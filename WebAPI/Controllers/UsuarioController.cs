ba89a13d603961a4e55e6642184800c06f50fbf1
Bruno Carvalho <seu-email@exemplo.com>
2025-07-18 10:30:59 -0300

    Subindo API básica

---
+++ WebAPI/Controllers/UsuarioController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+[ApiController]
+[Route("[controller]")]
+public class UsuarioController : ControllerBase
+{
+	[HttpGet("ObterDataHoraAtual")]
+	public IActionResult ObterDataAtual()
+	{
+		var obj = new
+		{
+			Data = DateTime.Now.ToLongDateString(),
+			Hora = DateTime.Now.ToShortTimeString()
+		};
+
+		return Ok(obj);
+		
+	}
+
+    [HttpGet("Apresentar/{nome}")]
+    public IActionResult Apresentar(string nome)
+	{
+		var mensagem = $"Olá {nome} é um prazer ter você na nossa API, seja muito bem vindo!";
+
+		return Ok(new { mensagem });
+	}
+}
